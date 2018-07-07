391521c06c3a16c27f83826c5cc8232564db27b0
beatorizu <betrizaf@gmail.com>
2018-07-07 17:56:35 -0300

    File-Removal as a Service with Mock Patch
    Test rm method of object.

--- controller/core.py
+++ controller/core.py
@@ -5,6 +5,9 @@ import os
 import os.path
 
 
-def rm(filename):
-    if os.path.isfile(filename):
-        os.remove(filename)
+class RemovalService(object):
+    """A service for removing objects from the filesystem."""
+
+    def rm(self, filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
--- controller/test/test_rm.py
+++ controller/test/test_rm.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-from controller.core import rm
+from controller.core import RemovalService
 
 from unittest import mock, TestCase
 
 
-class RmTestCase(TestCase):
+class RemovalServiceTestCase(TestCase):
 
     @mock.patch('controller.core.os.path')
     @mock.patch('controller.core.os')
     def test_rm(self, mock_os, mock_path):
+        # Instantiate our service
+        reference = RemovalService()
 
         # Set up the mock
         mock_path.isfile.return_value = False
 
-        rm('any path')
+        reference.rm('any path')
 
         # Test that the remove call was NOT called
         self.assertFalse(mock_os.remove.called,
@@ -25,7 +27,7 @@ class RmTestCase(TestCase):
         # Make the file 'exist'
         mock_path.isfile.return_value = True
 
-        rm('any path')
+        reference.rm('any path')
 
         # Test that rm called os.remove with the right parameters
         mock_os.remove.assert_called_with('any path')
