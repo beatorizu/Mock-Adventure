7d4195407ae3079678592ff83c52867306906808
beatorizu <betrizaf@gmail.com>
2018-07-07 19:56:46 -0300

    Mocking Instance Methods
    Testing UploadService dependency.

--- controller/core.py
+++ controller/core.py
@@ -11,3 +11,12 @@ class RemovalService(object):
     def rm(self, filename):
         if os.path.isfile(filename):
             os.remove(filename)
+
+
+class UploadService(object):
+
+    def __init__(self, removal_service):
+        self.removal_service = removal_service
+
+    def upload_complete(self, filename):
+        self.removal_service.rm(filename)
--- controller/test/test_rm.py
+++ controller/test/test_rm.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-from controller.core import RemovalService
+from controller.core import RemovalService, UploadService
 
 from unittest import mock, TestCase
 
@@ -31,3 +31,21 @@ class RemovalServiceTestCase(TestCase):
 
         # Test that rm called os.remove with the right parameters
         mock_os.remove.assert_called_with('any path')
+
+
+class UploadServiceTestCase(TestCase):
+
+    @mock.patch.object(RemovalService, 'rm')
+    def test_upload_complete(self, mock_rm):
+        # Build our depedencies
+        removal_service = RemovalService()
+        reference = UploadService(removal_service)
+
+        # Call upload_complete, which should, in turn, call `rm`:
+        reference.upload_complete('my uploaded file')
+
+        # Check that it called the rm method of any RemovalService
+        mock_rm.assert_called_with('my uploaded file')
+
+        # Check that it called the rm method of __our__ removal_service
+        removal_service.rm.assert_called_with('my uploaded file')
